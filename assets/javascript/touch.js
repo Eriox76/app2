e578f20cbe9ed1c32bba6f11a2363ee6a70f6ada
Quentin Brunet <brunet_quentin@outlook.fr>
2024-02-08 16:26:13 +0100

    modification

--- assets/javascript/touch.js
+++ assets/javascript/touch.js
@@ -1,6 +1,7 @@
 var touch = document.getElementById("touch");
-
+var debug = document.getElementById("debug");
 var hammertime = new Hammer(document.body, {});
+
 var ofLeft = touch.offsetLeft;
 var ofTop = touch.offsetTop;
 hammertime.on('pan pinch', function (event) {
@@ -9,6 +10,7 @@ hammertime.on('pan pinch', function (event) {
     let distance = event.distance;
     let additionalEvent = event.additionalEvent;
     console.log(additionalEvent, distance);
+    debug.innerText = additionalEvent;
     // "panright" "panleft" "panup" "pandown"
     switch (additionalEvent) {
         case "panright":
